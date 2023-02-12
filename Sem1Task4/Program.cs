a41714e91076c4044a4d408284f18f0443c029f5
igorsh81 <107439901+igorsh81@users.noreply.github.com>
2023-02-13 00:52:46 +0300

    Sem1HW

---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача №4
+//  Напишите программу, которая принимает на вход три числа 
+//  и выдаёт максимальное из этих чисел.
+
+// Запрашиваем число и преобразуем
+Console.WriteLine ("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите третье число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+// Выполняем условие и выводим ответ
+
+if (numberA > numberB && numberA > numberC)
+{
+    Console.WriteLine ("Максимальное число" + " " + numberA);
+}
+else if (numberB > numberA && numberB > numberC)
+{
+    Console.WriteLine ("Максимальное число" + " " + numberB);
+}
+else if (numberC > numberA && numberC > numberB)
+{
+    Console.WriteLine ("Максимальное число" + " " + numberC);
+}
---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача №8
+
+
+// Запрашиваем число 
+
+Console.WriteLine("Введите число : ");
+
+// Преобразуем переменную 
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+// Назначаем переменную
+int index = 1;
+
+// Выполняем условие и выводим результат
+
+while(index <= num)
+
+    if(index % 2 ==0)
+ {Console.Write(index + "  ");
+       index++;
+ }
+else
+{
+     index++;
+}
