dfacb9abbbb576b03bc804fe584b65d6becd1183
igorsh <igorsh1981@gmail.com>
2023-03-05 18:32:03 +0300

    Sem6

---
+++ Sem6Task44/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача №44
+// Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+// Первые два числа Фибоначчи: 0 и 1.
+
+int ReadDataToInt(string msg)
+{
+    Console.Write(msg);
+    return (int.Parse(Console.ReadLine() ?? "0"));
+}
+string FiboNum(int num)
+{
+    string res = "0 1";
+    int first = 0; int last = 1; 
+    for (int i = 2; i < num; i++)
+    {
+        res = res + " " + (first + last).ToString();
+        (first, last) = (last, first + last);
+    }
+    return res;
+}
+int num = ReadDataToInt("Enter a number of Fibonacci elements: ");
+Console.WriteLine(FiboNum(num));
