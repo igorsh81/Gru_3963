7ef67a12ce6911335dfceabe22458bcbcc21bcc3
igorsh <igorsh1981@gmail.com>
2023-03-08 02:21:40 +0300

    Sem7

---
+++ Sem7Task48/Program.cs
@@ -0,0 +1,30 @@
+﻿int[,] Gen2DArrayMod(int countRows, int countColumn)
+{
+    int[,] arr = new int[countRows, countColumn];
+
+    for (int i = 0; i < countRows; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            arr[i, j] = i + j;
+        }
+    }
+
+    return arr;
+}
+
+void Print2DArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] testArr = Gen2DArrayMod(8, 9);
+Print2DArr(testArr);
+
---
+++ Sem7Task49/Program.cs
@@ -0,0 +1,74 @@
+﻿int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+int[,] Gen2DArr(int countRow, int countColumn, int min, int max)
+{
+    // Корректировка входных данных
+    if (min > max)
+    {
+        int buf = min;
+        min = max;
+        max = buf;
+    }
+    Random rnd = new Random();
+    int[,] arr = new int[countRow, countColumn];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            arr[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+
+void Print2DArr(int[,] arr)
+{
+    ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.ForegroundColor = col[new Random().Next(0,16)];
+            Console.Write(arr[i, j] + "\t");
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,] RemastArr(int[,] arr, int countRow, int countColumn)
+{
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            if((i%2 == 0) && (j%2 ==0))
+            {
+                arr[i, j] = arr[i, j]*arr[i, j];
+            }
+        }
+    }
+    return arr;
+}
+
+int row = ReadData("Введите количество строк ");
+int column = ReadData("Введите количество столбцов ");
+int[,] testArr = Gen2DArr(row, column, 10, 100);
+Print2DArr(testArr);
+int[,] resArr = RemastArr(testArr, row, column);
+Print2DArr(resArr);
+
+
+
+
