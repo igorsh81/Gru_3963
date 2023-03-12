e40be39c062d18e74447d5bf8a40c2ffb82a3061
igorsh <igorsh1981@gmail.com>
2023-03-12 20:13:20 +0300

    Sem9HW

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 66: 
+// Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+
+// Запрашиваем переменные
+
+int ReadData(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+int m = ReadData("Введите m: ");
+int n = ReadData("Введите n: ");
+int temp = m;
+
+// Назначаем переменные
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+PrintSumm(m, n, temp=0);
+
+// Выполняем условие , выводим результат
+
+void PrintSumm(int m, int n, int summ)
+{
+  summ = summ + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма элементов = {summ} ");
+    return;
+  }
+  PrintSumm(m, n - 1, summ);
+}
+
