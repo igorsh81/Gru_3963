fb814b7077bc11f7c04d48c62d43d4deb1683b58
igorsh <igorsh1981@gmail.com>
2023-03-12 14:06:45 +0300

    Sem9

---
+++ Sem9Task67/Program.cs
@@ -0,0 +1,19 @@
+﻿int ReadData(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintData(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+int SumDigitRec(int num)
+{
+    if (num == 0) return 0;
+    else return num%10 + SumDigitRec(num/10);
+}
+
+int inputNum = ReadData("Enter your number: ");
+PrintData($"{SumDigitRec(inputNum)}");
